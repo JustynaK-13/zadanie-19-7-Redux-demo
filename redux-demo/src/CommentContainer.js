8e7ab58a5170e92df06a038da4391d37d779e3e9
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-06-20 14:52:46 +0200

    Delete errors

--- redux-demo/src/CommentContainer.js
+++ redux-demo/src/CommentContainer.js
@@ -2,16 +2,10 @@ import {connect} from 'react-redux';
 import Comment from './Comment';
 import {thumbUpComment} from './actions';
 import {thumbDownComment} from './actions';
-import {removeComment} from './actions';
-import {editComment} from './actions';
-import {addComment} from './actions';
 
 const mapDispatchToProps = dispatch => ({
   thumbUpComment: (id) => dispatch(thumbUpComment(id)),
   thumbDownComment: (id) => dispatch(thumbDownComment(id)),
-  removeComment: (id) => dispatch(removeComment(id)),
-  editComment: (text, id) => dispatch(editComment(text, id)),
-  addComment: (text) => dispatch(addComment(text))
-});
+ });
 
 export default connect(null, mapDispatchToProps)(Comment);
